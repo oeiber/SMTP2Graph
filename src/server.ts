b56dd61797b665acba2154e9988a108d9d5be15b
oeiber <46045177+oeiber@users.noreply.github.com>
2025-11-12 19:44:19 +0100

    fix build errors; undo unwanted code formatting

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,16 @@ import { log } from './classes/Logger';
 import { MailQueue } from './classes/MailQueue';
 import { SMTPServer } from './classes/SMTPServer';
 
-if (process.argv.includes('-v') || process.argv.includes('--version')) // We're asked for our version?
+if(process.argv.includes('-v') || process.argv.includes('--version')) // We're asked for our version?
     console.log(`SMTP2Graph v${VERSION}`);
-else {
-    (async () => {
+else
+{
+    (async ()=>{
         // Validate the config before continuing
         try {
             Config.validate();
-        } catch (error) {
-            await log('error', `Invalid config. ${String(error)}`, { error });
+        } catch(error) {
+            await log('error', `Invalid config. ${String(error)}`, {error});
             process.exit(1);
         }
 
@@ -19,19 +20,19 @@ else {
         const server = new SMTPServer(queue);
         try {
             await server.listen();
-        } catch (error) {
-            log('error', `Failed to start SMTP server. ${String(error)}`, { error });
+        } catch(error) {
+            log('error', `Failed to start SMTP server. ${String(error)}`, {error});
             process.exit(1);
         }
     })();
 }
 
 // Exit with code 0 on Ctrl+C
-process.on('SIGINT', () => {
+process.on('SIGINT', ()=>{
     process.exit(0);
-}
-// Exit with code 0 when container is stopped
-process.on('SIGTERM', () => {
-    process.exit(0);
+});
 
+// Exit with code 0 when container is stopped
+process.on('SIGTERM', ()=>{
+    process.exit(0);
 });
